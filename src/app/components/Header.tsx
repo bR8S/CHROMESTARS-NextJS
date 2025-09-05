bb720e168a99ee7aa663e9faa9fbd16a717ca2bd
guillermo mata <guillermomata@macbookpro.attlocal.net>
2025-09-05 11:14:36 -0700

    feat: working on header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,24 @@
+import Image from "next/image";
 
 export default function Header(){
     return (
-        <> HEADER </>
+        <> 
+            <header>
+                <nav>
+                    <a href="/" className="logo">
+                        <Image src="/chromestars-stars-logo.png" height="32" width="272" alt="Chromestars logo" />
+                    </a>
+                    <div className="nav__right">
+                        <a className="nav__hover" href="/event">EVENTS</a>
+                        <a className="nav__hover" href="/history">HISTORY</a>
+                        <a className="nav__hover" href="/">LEADERBOARD</a>
+                        <div className="nav-right__search nav__hover">SEARCH</div>
+                        <a href="/account">
+                            <Image className="account" src="/chromestars-badge-account.png" height="40" width="40" alt="Account page logo" />
+                        </a>
+                    </div>
+                </nav>
+            </header>
+        </>
     )
 }
